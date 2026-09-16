7360de1074779543564df7df1b325cba8a2af2ce
Raha <rahasani6186@gmail.com>
2026-09-16 23:27:47 +0330

    Add chaining with box_number and full reset with c

--- calculator.py
+++ calculator.py
@@ -1,10 +1,12 @@
-def calculate(con_num_1, con_num_2, operator):
+def calculate(con_num_1, operator, con_num_2):
     if operator == "+":
         return con_num_1 + con_num_2
-    elif operator =="-":
+    elif operator == "-":
         return con_num_1 - con_num_2
     elif operator == "*":
         return con_num_1 * con_num_2
+    elif operator == "**":
+        return con_num_1 ** con_num_2
     elif operator == "/":
         return con_num_1 / con_num_2
     elif operator == "%":
@@ -26,17 +28,21 @@ def show_history():
 
 
 history = []
-
+box_number = None
 while True:
-    
-    num_1 = input("Please enter one number!")
+
+    if box_number == None: 
+        num_1 = input("Please enter one number!")
+    else:
+        num_1 = box_number
+        
     if num_1 == "q" or num_1 == "Q":
         print("Goodbye")
         break
     if num_1 == "h" or num_1 == "H":
         show_history()
         continue
-
+    
     if num_1 == "delete":
         if len(history) == 0:
             print("History is empty")
@@ -56,18 +62,13 @@ while True:
                     break
                 else:
                     print("This number doesn't exist! Please try again.")
-        continue
-    if num_1 == "c" or num_1 == "C":
+            continue 
+    if num_1 == "c" or num_1 == "C": 
         history.clear()
+        box_number = None 
         print("History deleted!")
         continue
-    num_2 = input("Please enter another number!")
-    if num_2 == "q" or num_2 == "Q":
-        print("Goodbye")
-        break
-    if num_2 == "h" or num_2 == "H":
-        show_history()
-        continue
+    
     operator = input("What operator do you want ?")
     if operator == "q" or operator == "Q":
         print("Goodbye")
@@ -75,14 +76,51 @@ while True:
     if operator == "h" or operator == "H":
         show_history()
         continue
+    if operator == "delete":
+        if len(history) == 0:
+            print("History is empty")
+        else:
+            show_history()
+            while True:
+                num = input("Which one number do you wanted to deleted ?")
+                try:
+                    index = int(num)
+                except ValueError: 
+                    print("Input is invalid . Just number .")
+                    continue
+
+                if 1 <= index <= len(history):
+                    history.pop(index - 1)
+                    print(f"Item number {index} deleted.")
+                    break
+                else:
+                    print("This number doesn't exist! Please try again.")
+            continue 
+    if operator == "c" or operator == "C":
+        history.clear()
+        box_number = None
+
+        print("History deleted!")
+        continue
+
+    num_2 = input("Please enter another number!") 
+    if num_2 == "q" or num_2 == "Q":
+        print("Goodbye")
+        break
+    if num_2 == "h" or num_2 == "H":
+        show_history()
+        continue
     try:
         con_num_1 = float(num_1)
         con_num_2 = float(num_2)
-        total = calculate(con_num_1, con_num_2, operator)
+        total = calculate(con_num_1, operator, con_num_2)
+
         if total is None:
             print("Operator is invalid")
         else:
             total = pretty(total)
+            box_number = total
+
             print(total)
             history.append(f"{pretty(con_num_1)} {operator} {pretty(con_num_2)} = {total}")
     except ZeroDivisionError:
